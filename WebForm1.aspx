41083f9d66b06c8bd3b767da529eace4751fb8c2
uknow545 <uknow_545@hotmail.com>
2022-11-15 09:13:18 +0800

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" Text="Button" OnClick="Unnamed1_Click"></asp:Button>
+            <asp:GridView runat="server" ID="gv1"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            proc();
+        }
+        private void proc()
+        {
+           HtmlWeb webClient = new HtmlWeb();
+           HtmlDocument linedoc = webClient.Load("https://twur.cpami.gov.tw/zh/merchant/announcement/0");  //載入網址資料                                                                                           // HtmlDocument linedoc = webClient.Load("https://www.google.com/search?q=都更&start=1");  
+           HtmlNodeCollection linelist = linedoc.DocumentNode.SelectNodes("/html[1]/body[1]/div[1]/div[1]/div[5]/table[1]//a"); //抓取Xpath資料
+           List<string> lsUrl = new List<string>();
+           List<string> lsUrldtl = new List<string>();
+
+           foreach (var item in linelist)
+           {
+               // if (IsNumeric(item.InnerText))                      
+               if (item.Attributes["title"] != null)
+               {
+                   lsUrl.Add(item.Attributes["href"].Value + ";" + item.Attributes["title"].Value);
+                   linedoc = webClient.Load(item.Attributes["href"].Value);  //載入網址資料
+                   lsUrldtl.Add(linedoc.DocumentNode.SelectNodes("/html[1]/body[1]/div[1]/div[1]").FirstOrDefault().OuterHtml); //抓取Xpath資料
+
+               }
+
+           }
+           gv1.DataSource = lsUrldtl;
+           gv1.DataBind();
+        }
+
+    }
+    }
